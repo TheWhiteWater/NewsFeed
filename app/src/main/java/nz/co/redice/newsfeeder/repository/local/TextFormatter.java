5a32e2c54d1ad7d190aa592b049695b305977c56
red ice <redicenone@gmail.com>
2020-05-09 13:39:56 +1200

    flattering

--- app/src/main/java/nz/co/redice/newsfeeder/repository/local/TextFormatter.java
+++ app/src/main/java/nz/co/redice/newsfeeder/repository/local/TextFormatter.java
@@ -2,29 +2,27 @@ package nz.co.redice.newsfeeder.repository.local;
 
 public class TextFormatter {
 
-
-    public static String getSourceTag(String source) {
-        if (source.contains(".co.nz")) {
-            String[] subs = source.split(".co.nz");
-            source = subs[0];
-        }
-        return source;
-    }
+//
+//    public static String getSourceTag(String source) {
+//        if (source.contains(".co.nz")) {
+//            String[] subs = source.split(".co.nz");
+//            source = subs[0];
+//        }
+//        return source;
+//    }
 
     public static String cutOfSourceName(String title, String tag) {
 
-        if (tag.contains(".co")) {
-            String[] subs = tag.split(".co");
-            tag = subs[0];
-        }
+        StringBuilder sb = new StringBuilder();
 
-        String subs[] = title.split(" - ");
-        if (subs[1].contains(tag) ||
-                subs[1].contains("1News") ||
-                subs[1].contains("New Zealand Herald")
-        ) {
-            title = subs[0];
+        String[] subs = title.split("-");
+        sb.append(subs[0]);
+        if (subs.length > 2 ) {
+            for (int i = 1; i < subs.length - 1; i++) {
+                sb.append(subs[i]);
+            }
+
         }
-        return title;
+        return sb.toString();
     }
 }
